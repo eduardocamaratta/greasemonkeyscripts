8043cc0307907b22de8320d6cee17bb29fdec145
Eduardo Camaratta <eduardocamaratta@gmail.com>
2017-01-05 09:52:39 -0200

    styles for fibonacci estimatives added to trello script

--- TrelloAddEstimativeBadge.user.js
+++ TrelloAddEstimativeBadge.user.js
@@ -22,19 +22,49 @@ if (window.self != window.top) {
  ** Badges **
  *********************************************************************************************************************/
 
+styles = `<style>
+.task-estimative {
+  background-color: #909090;
+  color: white;
+  border-radius: 4px;
+  font-size: 12px;
+  width: 20px;
+  text-align: center;
+  font-weight: bold;
+  border: 1px solid #d0d0d0;
+}
+.f0  { background-color:#4040FF; }
+.f1  { background-color:#00E090; color: black; }
+.f2  { background-color:#40A000; }
+.f3  { background-color:#B0D000; color: black; }
+.f5  { background-color:#F0F000; color: black; }
+.f8  { background-color:#FF6000; }
+.f13 { background-color:#D02020; }
+</style>`;
+
 var addEstimativeBadges = function() {
-  $("<style>.task-estimative, .t-shirt-p, .t-shirt-m, .t-shirt-g {background-color: #909090;color: white;border-radius: 4px;font-size: 12px;width: 20px;text-align: center;font-weight: bold;border: 1px solid #d0d0d0;}.t-shirt-p {background-color: #009933;}.t-shirt-m {background-color: #ffff00; color: #404040;}.t-shirt-g {background-color: #cc0000;}</style>").appendTo('head');
+  $(styles).appendTo('head');
 
   $('.list-card-details').each(function(i, c) {
     var title = $(c).find('.list-card-title').text();
-    var estimative = title.match(/^(\#\d+)?(\(.\))?(.*)$/)[2];
+    var estimative = title.match(/^(\#\d+)?(\(\w+\))?(.*)$/)[2];
     if(!estimative) {
       return;
     }
-    var value = estimative.match(/^\((.)\)$/)[1];
-    var estimativeClass = {'P': 't-shirt-p', 'M': 't-shirt-m', 'G': 't-shirt-g'}[value];
-    estimativeClass = estimativeClass ? estimativeClass : '.task-estimative';
-    $(c).find('.js-plugin-badges').append('<div class="badge ' + estimativeClass + '">' + value + '</div>');
+    var value = estimative.match(/^\((\w+)\)$/)[1];
+    var estimativeClass = {
+      'P': 'f2',
+      'M': 'f5',
+      'G': 'f13',
+      '0': 'f0',
+      '1': 'f1',
+      '2': 'f2',
+      '3': 'f3',
+      '5': 'f5',
+      '8': 'f8',
+      '13': 'f13'}[value];
+    estimativeClass = estimativeClass ? ' ' + estimativeClass : '';
+    $(c).find('.js-plugin-badges').append('<div class="badge task-estimative' + estimativeClass + '">' + value + '</div>');
   });
 };
 
