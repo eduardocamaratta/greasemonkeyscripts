ad8c57f2498e792ae7f1bfcfa2fed727d7ae483f
Eduardo Camaratta <eduardocamaratta@gmail.com>
2017-02-25 16:22:48 -0300

    Added a script which automatically redirects ItauCinemas to the proper landing page, avoiding an annoying 404

---
+++ RedirectItauCinemas.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        RedirectItauCinemas
+// @namespace   gmcamaratta
+// @description Redirect the Itau Cinemas automatically
+// @include     http://www.itaucinemas.com.br/404.html
+// @version     1
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+window.location.href = "http://www.itaucinemas.com.br/filmes";
